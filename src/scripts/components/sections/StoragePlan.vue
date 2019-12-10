7f067bd1a0b8c72306776d784aa80406cb932bf6
Jake Gornall <jakegornall@Jakes-MacBook-Pro.local>
2019-12-10 16:09:16 -0500

    Gornall-9570: Finish plan options section

--- src/scripts/components/sections/StoragePlan.vue
+++ src/scripts/components/sections/StoragePlan.vue
@@ -14,10 +14,14 @@
         p.storage-plan__plan-box__subtitle {{ plan.subtitle | unescape }}
         ul.storage-plan__plan-box__features
           li.storage-plan__plan-box__feature
-            icon(name="checkmark")
+            icon(name="checkmark" size="13px")
             | {{ plan.feature_1 | unescape }}
-          li.storage-plan__plan-box__feature {{ plan.feature_2 | unescape }}
-          li.storage-plan__plan-box__feature {{ plan.feature_3 | unescape }}
+          li.storage-plan__plan-box__feature
+            icon(name="checkmark" size="13px")
+            | {{ plan.feature_2 | unescape }}
+          li.storage-plan__plan-box__feature
+            icon(name="checkmark" size="13px")
+            | {{ plan.feature_3 | unescape }}
         .storage-plan__plan-box__subcopy(v-if="plan.subcopy" v-html="plan.subcopy")
 
   p.storage-plan__disclaimer {{ disclaimer | unescape }}
@@ -63,6 +67,12 @@ export default {
   &__container {
     display: flex;
     flex-direction: column;
+
+    @include tablet-up {
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+    }
   }
 
   &__title {
@@ -70,6 +80,12 @@ export default {
     line-height: 32px;
     font-weight: bold;
     margin: 0 0 16px 0;
+
+    @include tablet-up {
+      font-size: 32px;
+      line-height: 40px;
+      margin-bottom: 12px;
+    }
   }
 
   &__subtitle {
@@ -78,12 +94,23 @@ export default {
     font-weight: 500;
     color: $color--primary--light;
     margin: 0 0 16px 0;
+
+    @include tablet-up {
+      font-size: 24px;
+      line-height: 32px;
+      margin-bottom: 24px;
+    }
   }
 
   &__desc {
     font-size: 14px;
     line-height: 20px;
     margin-bottom: 20px;
+
+    @include tablet-up {
+      font-size: 16px;
+      line-height: 22px;
+    }
   }
 
   &__disclaimer {
@@ -91,6 +118,11 @@ export default {
     font-size: 10px;
     line-height: 16px;
     color: $text--subtitle;
+    
+    @include tablet-up {
+      text-align: center;
+      margin-top: 56px;
+    }
   }
 
   &__copy,
@@ -98,8 +130,22 @@ export default {
     text-align: center;
   }
 
+  &__copy {
+    max-width: 580px;
+
+    @include tablet-up {
+      padding-right: 42px;
+      text-align: left;
+    }
+  }
+
   &__plan-box {
     margin: 20px 25px;
+
+    @include tablet-up {
+      margin: 0 10px;
+      align-self: flex-end;
+    }
 
     &__img {
       max-height: 126px;
@@ -129,7 +175,7 @@ export default {
       list-style: none;
       text-align: left;
       margin: 0 auto;
-      max-width: 165px;
+      max-width: 175px;
     }
 
     &__feature {
@@ -140,19 +186,30 @@ export default {
       &:last-child {
         margin-bottom: 0;
       }
+
+      .icon {
+        position: relative;
+        top: 4px;
+        margin-right: 16px;
+      }
     }
 
     &__subcopy {
       font-size: 12px;
       line-height: 13px;
       font-weight: 500;
-      margin-bottom: 25px;
+      margin-top: 26px;
     }
 
     &__info {
       background-color: $color-white;
       border-top: 2px solid #000;
       padding: 24px 15px 32px 15px;
+
+      @include tablet-up {
+        height: 249px;
+        width: 297px;
+      }
     }
   }
 }
